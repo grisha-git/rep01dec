82c92ff10edc5d0c5f57191e4663b69468b6bcf0
grisha-git <donkeevgrisa@gmail.com>
2025-12-01 17:16:17 +0300

    Class Dot

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ namespace top{
     int x, y;
   };
   struct IDraw{
-    virtual p_t begin() = 0;
-    virtual p_t next(p_t) = 0;
+    virtual p_t begin() const = 0;
+    virtual p_t next(p_t) const = 0;
   };
   bool operator==(p_t a, p_t b){
     return a.x == b.x && a.y==b.y;
@@ -15,8 +15,25 @@ namespace top{
   {
     return !(a == b);
   }
+  struct Dot:IDraw{
+    p_t begin() const override;
+    p_t next(p_t) const override;
+    p_t o;
+    Dot(int x, int y);
+  };
 }
 
+top::Dot::Dot(int x, int y):
+  IDraw(),
+  o{x, y}
+{}
+top::p_t top::Dot::begin() const{
+  return o;
+
+}
+top::p_t top::Dot::next(p_t) const{
+  return begin();
+}
 int main(){
   using namespace top;
 }
